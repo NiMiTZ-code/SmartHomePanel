700df6b62216ee614c98c2b91ff34efe325f8d80
Dawid Niemiec <dn305391@student.polsl.pl>
2025-02-05 15:26:42 +0100

    New classes, small changes in thermostat to accomodate them

---
+++ heater.h
@@ -0,0 +1,21 @@
+#ifndef HEATER_H
+#define HEATER_H
+
+#include <QObject>
+#include "actuator.h"
+
+class Heater : public Actuator
+{
+    Q_OBJECT
+public:
+    explicit Heater(QObject *parent = nullptr);
+    void toggle() override;
+    void sendCommand() override;
+private slots:
+    void onUIToggle();
+   // void onTurnHeater_ON();
+signals:
+    void heaterToggled(bool isOn);
+};
+
+#endif // HEATER_H
